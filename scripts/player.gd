e98ec46df057385e99fb51d0d9e8857db101a8b3
Daniel G. <nuclearrocketcar@gmail.com>
2025-03-28 13:24:41 -0400

    added jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,10 +4,17 @@ extends CharacterBody2D
 const RUN_SPEED = 120
 const RUN_DECEL = 12
 
+const JUMP_VELOCITY = -290
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 func _process(delta: float) -> void:
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 	
 	var direction = Input.get_axis("left", "right")
 	sprite_flip(direction)
