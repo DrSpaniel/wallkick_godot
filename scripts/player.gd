4fda3b818ffc4de40e83255f410ba01e2f29bc7f
Daniel G. <nuclearrocketcar@gmail.com>
2025-03-28 14:19:14 -0400

    ruined the fun with only 2 wallkicks

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,10 +10,9 @@ const WALL_KICK_SPEED = 200  # Horizontal speed when kicking off a wall
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var kickCount = 0 #used to only allow 2 kicks per wall until you touch ground
 
 func _process(delta: float) -> void:
-	
-	#print(velocity.x)
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -21,13 +20,18 @@ func _process(delta: float) -> void:
 		if is_on_wall():
 			if Input.is_action_just_pressed("ui_accept"): #when the player is jumping and touching a wall, then jumps again
 				print("beep!")
-				var collision = get_last_slide_collision()
-				if collision:		#if the player hits the wall
-					var wall_normal = collision.get_normal()	#negates the need to care about whether the player is kicking from left or right.
-					velocity.x = wall_normal.x * WALL_KICK_SPEED
-					velocity.y = JUMP_VELOCITY * 0.8  #makes the kick weaker than an actual jump
 				
-	
+				print(kickCount)
+				if kickCount < 2:		#prevents cheating cause im a sore loser
+					var collision = get_last_slide_collision()
+					if collision:		#if the player hits the wall
+						var wall_normal = collision.get_normal()	#negates the need to care about whether the player is kicking from left or right.
+						velocity.x = wall_normal.x * WALL_KICK_SPEED
+						velocity.y = JUMP_VELOCITY * 0.8  #makes the kick weaker than an actual jump
+				kickCount = kickCount+1
+				
+	if is_on_floor():
+		kickCount = 0
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
