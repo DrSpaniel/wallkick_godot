cb5c615a3d477e14636bc4406ea122511c867a1f
Darcy Mazloum <damazinator11@gmail.com>
2025-03-28 12:33:12 -0400

    Added movement
    Left right movement as well as sprite flip for direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,30 @@
 extends CharacterBody2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+const RUN_SPEED = 120
+const RUN_DECEL = 12
 
 func _process(delta: float) -> void:
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-		
+	var direction = Input.get_axis("left", "right")
+	sprite_flip(direction)
 	
-	# var direction = Input.get_axis("left", "right")
+	if direction:
+		# Accel/decel to normal speed.
+		velocity.x = move_toward(velocity.x, RUN_SPEED*direction, RUN_DECEL)
+		# If we are not moving.
+	else:
+			# Decelerate to 0.
+		velocity.x = move_toward(velocity.x, 0, RUN_DECEL)
 	
 	move_and_slide()
+	
+# Function that handles the flipping of the sprite based on direction.
+func sprite_flip(direction):
+	if direction > 0:
+		animated_sprite.flip_h = false # right
+	elif direction < 0:
+		animated_sprite.flip_h = true
